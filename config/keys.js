ae51835440aa709f9759c16c12740ef35a6f4bad
bags0104 <bhakti04ganesh@gmail.com>
2020-12-26 20:11:42 +0530

    Sign in Sign up

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOURI : "mongodb+srv://bhakti:cbwXuqUiTcK6xVBY@cluster0.ykuj1.mongodb.net/FARMICHAT?retryWrites=true&w=majority",
+    JWT_SECRET : "skjdfnkjfhefvesrfewiuridlkwdkwwjknds"
+}
